410dc30f9772cd8f9a838f9427e1669e1f4a74d4
E Eue <elena-urra@hotmail.com>
2020-05-12 18:58:49 +0200

    inicio sesion

--- PistasPadel/PistasPadel/Pages/Index.cshtml.cs
+++ PistasPadel/PistasPadel/Pages/Index.cshtml.cs
@@ -5,12 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using PistasPadel.Models;
 
 namespace PistasPadel.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public Cliente Cliente { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
